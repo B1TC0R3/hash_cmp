470c4676a18ac46ef1db2de13417349831979351
B1TC0R3 <github@b1tc0r3.com>
2022-07-29 17:22:02 +0200

    Add result return statement

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,27 @@ fn check_args(args: Vec<String>) -> Result<(), Box<dyn Error>>{
         process::exit(0);
     }
 
-    if args.len() == 4 {
+    if args.len() == 3 {
         return Ok(());
     }
     Err("Invalid Arguments!".into())
+}
+
+fn hash_cmp(a: String, mut b: String) -> (bool, String) {
+    if a.len() != b.len() {
+        return (false, "Hash values are not equal in length.".to_string());
+    } else {
+        let mut is_equal: bool = true;
+        for i in 0..a.len() {
+            if a.chars().nth(i).unwrap() != b.chars().nth(i).unwrap() {
+                is_equal = false;
+            } 
+        }
+       
+        let result = format!("{}\n{}", a, b);
+    
+        return (is_equal, result);
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>>{
