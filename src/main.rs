669752395463a81b324587f82594f42c509595f0
B1TC0R3 <thomas@gingele.de>
2022-07-29 21:33:58 +0200

    Add file hash256 calculation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
-use std::process;
+use std::{env, process, fs};
 use std::error::Error;
+use sha2::{Sha256, Digest};
 
 fn print_help() {
     println!("Usage: sha-cmp <method> <input-file> <sha>");
@@ -18,6 +18,15 @@ fn check_args(args: Vec<String>) -> Result<(), Box<dyn Error>>{
     Err("Invalid Arguments!".into())
 }
 
+fn get_file_hash256(path: String) -> String {
+    let mut hasher = Sha256::new();
+    let mut file_content = fs::read_to_string(path).expect("Failed to read file");
+    hasher.update(&mut file_content);
+    let hash256 = hasher.finalize();
+
+    format!("{:x}", hash256)
+}
+
 fn hash_cmp(a: String, b: String) -> (bool, String) {
     if a.len() != b.len() {
         return (false, "Hash values are not equal in length.".to_string());
@@ -29,13 +38,13 @@ fn hash_cmp(a: String, b: String) -> (bool, String) {
         for i in 0..a.len() {
             cmp_char = b.chars().nth(i).unwrap();
             if a.chars().nth(i).unwrap() != cmp_char {
-                cmp_marker = format!("{}\x1b[31m{}\x1b[0m", cmp_marker, cmp_char); 
+                cmp_marker = format!("{}\x1b[31m{}\x1b[0m", cmp_marker, cmp_char);
                 is_equal = false;
             } else {
                 cmp_marker = format!("{}{}", cmp_marker, cmp_char);
             }
         }
-       
+
         let result = format!("{}\n{}", a, cmp_marker);
         return (is_equal, result);
     }
@@ -48,14 +57,14 @@ fn main() -> Result<(), Box<dyn Error>>{
         _ => {}
     }
 
-    let hash_a: String = args[1].clone();
+    let hash_a: String = get_file_hash256(args[1].clone());
     let hash_b: String = args[2].clone();
 
     let (is_equal, msg) = hash_cmp(hash_a, hash_b);
     if is_equal {
         println!("\x1b[32mThe hash values are equal.\x1b[0m");
     } else {
-        println!("The hash values are not equal:\n{}", msg);
+        println!("Found inconsistency:\n{}", msg);
     }
 
     Ok(())
