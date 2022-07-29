11e489fc5e9b384e886a8f0941a75ba1638676a8
B1TC0R3 <github@b1tc0r3.com>
2022-07-29 17:37:39 +0200

    Finalize comparison method

--- src/main.rs
+++ src/main.rs
@@ -18,28 +18,44 @@ fn check_args(args: Vec<String>) -> Result<(), Box<dyn Error>>{
     Err("Invalid Arguments!".into())
 }
 
-fn hash_cmp(a: String, mut b: String) -> (bool, String) {
+fn hash_cmp(a: String, b: String) -> (bool, String) {
     if a.len() != b.len() {
         return (false, "Hash values are not equal in length.".to_string());
     } else {
         let mut is_equal: bool = true;
+        let mut cmp_marker: String = String::new();
+        let mut cmp_char: char;
+
         for i in 0..a.len() {
-            if a.chars().nth(i).unwrap() != b.chars().nth(i).unwrap() {
+            cmp_char = b.chars().nth(i).unwrap();
+            if a.chars().nth(i).unwrap() != cmp_char {
+                cmp_marker = format!("{}\033[34m{}\033[0m", cmp_marker, cmp_char); 
                 is_equal = false;
-            } 
+            } else {
+                cmp_marker = format!("{}{}", cmp_marker, cmp_char);
+            }
         }
        
         let result = format!("{}\n{}", a, b);
-    
         return (is_equal, result);
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = env::args().collect();
-    match check_args(args) {
+    match check_args(args.clone()) {
         Err(e) => return Err(e),
         _ => {}
+    }
+
+    let hash_a: String = args[1].clone();
+    let hash_b: String = args[2].clone();
+
+    let (is_equal, msg) = hash_cmp(hash_a, hash_b);
+    if is_equal {
+        println!("The hash values are equal!");
+    } else {
+        println!("The hash values are not equal!\n{}", msg);
     }
 
     Ok(())
