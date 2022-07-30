3c603a3bdd59b25c403878eca42bea1407b1117a
B1TC0R3 <thomas@gingele.de>
2022-07-30 19:28:27 +0200

    Fix quiet mode not working when '-q' is used

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use sha2::{Sha256, Digest};
 
 enum ExitCode {
     HashEqual = 0,
-    HashNotEqual = 99,
+    HashNotEqual = 255,
 }
 
 struct CmpResult {
@@ -30,15 +30,24 @@ fn print_quiet(cmp_result: CmpResult) {
     println!("{}", cmp_result.expected_hash);
 }
 
-fn parse_args(mut args: Vec<String>) -> Result<(String, String), Box<dyn Error>>{
+fn parse_args(mut args: Vec<String>) -> Result<(String, String, bool), Box<dyn Error>>{
+    let mut quiet = false;
+
     if args[1] == "-h" {
         print_help();
         process::exit(0);
     }
 
+    for arg in &args {
+        if arg == "-q" {
+            quiet = true;
+        }
+    }
+
     if args.len() >= 3 {
         return Ok((args.pop().unwrap(), 
-                   args.pop().unwrap())); 
+                   args.pop().unwrap(),
+                   quiet)); 
     }
     Err("Invalid Arguments!".into())
 }
@@ -78,13 +87,13 @@ fn hash_cmp(a: String, b: String) -> Result<CmpResult, CmpResult> {
             Ok(CmpResult {
                 msg: "Hashes are equal!".to_string(),
                 file_hash: a,
-                expected_hash: b,
+                expected_hash: cmp_marker,
             })
         } else {
             Err(CmpResult {
                 msg: "Hashes are not equal!".to_string(),
                 file_hash: a,
-                expected_hash: b,
+                expected_hash: cmp_marker,
             })
         }
     }
@@ -94,27 +103,29 @@ fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = env::args().collect();
     let file_hash: String;
     let expected_hash: String;
+    let quiet: bool;
 
     match parse_args(args) {
         Err(e) => return Err(e),
-        Ok((hash, file_path)) => {
+        Ok((hash, file_path, mode)) => {
             file_hash = get_file_hash256(file_path);
             expected_hash = hash;
+            quiet = mode;
         }
     }
 
     match hash_cmp(file_hash, expected_hash) {
         Ok(msg) => {
-            match env::var("-q") {
-                Ok(_) => print_quiet(msg),
-                Err(_) => print_verbose(msg),
+            match quiet {
+                true => print_quiet(msg),
+                false => print_verbose(msg),
             }
             process::exit(ExitCode::HashEqual as i32);
         },
         Err(msg) => {
-            match env::var("-q") {
-                Ok(_) => print_quiet(msg),
-                Err(_) => print_verbose(msg),
+            match quiet {
+                true => print_quiet(msg),
+                false => print_verbose(msg),
             }
             process::exit(ExitCode::HashNotEqual as i32);
         }
