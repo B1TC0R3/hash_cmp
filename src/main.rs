df456a13d9c963ee9dc12a325d299a0ac7e63997
Thorge Hemken <thorge@flo.rip>
2022-08-01 23:16:11 +0200

    added structopt for arg parsing and error_stack for better errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![deny(clippy::pedantic)]
 
+use error_stack::{Context, IntoReport, Result, ResultExt};
 use sha2::{Digest, Sha256};
-use std::error::Error;
-use std::{env, fs, io, process};
+use std::path::{Path, PathBuf};
+use std::{fs, io, process};
+use structopt::StructOpt;
 
 const COLOR_RED: &str = "\x1b[31m";
 const COLOR_WHITE: &str = "\x1b[0m";
@@ -22,53 +24,62 @@ struct Cmp {
     file_hash: String,
     expected_hash: String,
 }
-
-fn print_help() {
-    println!("Usage: hash-cmp [optional: -q] <input-file> <expected hash>");
-    println!("Parameters:");
-    println!("  -q: quiet mode");
-}
-
-fn print_verbose(cmp_result: &Cmp) {
-    println!("{}", cmp_result.msg);
-    println!("Found   :: {}", cmp_result.file_hash);
-    println!("Expected:: {}", cmp_result.expected_hash);
-}
-
-fn print_quiet(cmp_result: &Cmp) {
-    println!("{}", cmp_result.file_hash);
-    println!("{}", cmp_result.expected_hash);
-}
-
-fn parse_args(mut args: Vec<String>) -> Result<(String, String, bool), Box<dyn Error>> {
-    let mut quiet = false;
-
-    if args.len() == 1 {
-        print_help();
-        process::exit(1);
-    }
-
-    if args[1] == "-h" {
-        print_help();
-        process::exit(0);
-    }
-
-    for arg in &args {
-        if arg == "-q" {
-            quiet = true;
+impl Cmp {
+    fn print(&self, quiet: bool) {
+        if quiet {
+            self.print_quiet();
+        } else {
+            self.print_verbose();
         }
     }
 
-    if args.len() >= 3 {
-        return Ok((args.pop().unwrap(), args.pop().unwrap(), quiet));
+    fn print_verbose(&self) {
+        println!("{}", self.msg);
+        println!("Found   :: {}", self.file_hash);
+        println!("Expected:: {}", self.expected_hash);
     }
-    Err("Invalid Arguments!".into())
+
+    fn print_quiet(&self) {
+        println!("{}", self.file_hash);
+        println!("{}", self.expected_hash);
+    }
 }
 
-fn get_file_hash256(path: String) -> Result<String, Box<dyn Error>> {
+///  Calculate and compare hash values quickly and easily
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(parse(from_os_str))]
+    input_file: PathBuf,
+    expected_hash: String,
+    /// Prints less information
+    #[structopt(short, long)]
+    quiet: bool,
+}
+
+#[derive(Debug)]
+struct HashError;
+
+impl std::fmt::Display for HashError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Hash Error: Could not hash file...")
+    }
+}
+
+impl Context for HashError {}
+
+fn get_file_hash256(path: &Path) -> Result<String, HashError> {
     let mut hasher = Sha256::new();
-    let mut file = fs::File::open(path)?;
-    io::copy(&mut file, &mut hasher)?;
+
+    let mut file = fs::File::open(path)
+        .report()
+        .change_context(HashError)
+        .attach_printable(format!("Could not open '{}'", path.display()))?;
+
+    io::copy(&mut file, &mut hasher)
+        .report()
+        .change_context(HashError)
+        .attach_printable(format!("Could not read from '{}'", path.display()))?;
+
     let hash256 = hasher.finalize();
 
     Ok(format!("{:x}", hash256))
@@ -110,36 +121,21 @@ fn hash_cmp(a: String, b: String) -> CmpResult {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let file_hash: String;
-    let expected_hash: String;
-    let quiet: bool;
+fn main() {
+    let args = Args::from_args();
 
-    match parse_args(args) {
-        Err(e) => return Err(e),
-        Ok((hash, file_path, mode)) => {
-            file_hash = get_file_hash256(file_path)?;
-            expected_hash = hash;
-            quiet = mode;
-        }
-    }
+    let file_hash = match get_file_hash256(&args.input_file) {
+        Ok(f) => f,
+        Err(e) => panic!("{e:?}"),
+    };
 
-    match hash_cmp(file_hash, expected_hash) {
+    match hash_cmp(file_hash, args.expected_hash) {
         CmpResult::Equal(cmp) => {
-            if quiet {
-                print_quiet(&cmp);
-            } else {
-                print_verbose(&cmp);
-            }
+            cmp.print(args.quiet);
             process::exit(ExitCode::HashEqual as i32);
         }
         CmpResult::NotEqual(cmp) => {
-            if quiet {
-                print_quiet(&cmp);
-            } else {
-                print_verbose(&cmp);
-            }
+            cmp.print(args.quiet);
             process::exit(ExitCode::HashNotEqual as i32);
         }
     }
